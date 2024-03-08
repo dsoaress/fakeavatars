0513823f82943c3ac103d3d3f1fb9a98662f109c
Daniel Soares <daniel.soares@me.com>
2024-03-08 19:19:06 -0300

    fix: path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
+  const NEXT_PUBLIC_VERCEL_URL = process.env.NEXT_PUBLIC_VERCEL_URL;
+  const url = NEXT_PUBLIC_VERCEL_URL
+    ? "https://" + NEXT_PUBLIC_VERCEL_URL
+    : "http://localhost:3000";
+
   return (
     <main>
       <h1 className="text-3xl">Avatars</h1>
-      <img src="http://localhost:3000/avatars?gender=MALE&key=danielsss" alt="avatar" />
+      <img src={`${url}/avatars?id=1`} alt="avatar" width={1024} height={1024} />
     </main>
   );
 }
